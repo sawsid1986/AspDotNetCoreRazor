7976b6410b58fcc86c0c707d9e5a24bad8950d52
Siddhesh Sawant <sSawant@braveenergy.local>
2019-03-13 01:44:47 +1100

    Add project files.

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+
+        public IActionResult OnGet(int? restaurantId)
+        {
+            if (restaurantId.HasValue)
+            {
+                Restaurant = restaurantData.GetById(restaurantId.Value);
+                if (Restaurant == null)
+                    return RedirectToPage("./NotFound");
+            }
+            else
+                Restaurant = new Restaurant();
+
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+                return Page();
+            }
+            if (Restaurant.Id > 0)
+            {
+                Restaurant = restaurantData.Update(Restaurant);
+            }
+            else
+            {
+                Restaurant = restaurantData.Add(Restaurant);
+            }
+            restaurantData.Commit();
+            TempData["Message"] = "Restaurant saved!";
+            return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+        }
+    }
+}
